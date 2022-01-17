df6a1f323618b0d01b4532aa5fd8c562cba0892d
lexach91 <ronmctailor@protonmail.com>
2022-01-17 15:06:00 +0800

    Add display tasks to the user

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -7,7 +7,8 @@ from taskmanager.models import Task, Category
 
 @app.route("/")
 def home():
-    return render_template("tasks.html")
+    tasks = list(Task.query.order_by(Task.id).all())
+    return render_template("tasks.html", tasks=tasks)
 
 
 @app.route("/categories")
