61f5cd9afab78895df259e9ea120e242324fc196
lexach91 <ronmctailor@protonmail.com>
2022-01-17 11:53:03 +0800

    Add code to add new category to the database

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -1,4 +1,5 @@
-from flask import render_template
+from unicodedata import category
+from flask import render_template, request, redirect, url_for
 from taskmanager import app, db
 from taskmanager.models import Task, Category
 
@@ -14,4 +15,9 @@ def categories():
 
 @app.route("/add_category", methods=["GET", "POST"])
 def add_category():
+    if request.method == "POST":
+        category = Category(category_name=request.form.get("category_name"))
+        db.session.add(category)
+        db.session.commit()
+        return redirect(url_for("categories"))
     return render_template("add_category.html")
